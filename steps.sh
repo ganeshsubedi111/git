183731a2c2e45f9080776df1ee2d14f4ca489fc2
Ganesh subedi <73029563+ganeshsubedi111@users.noreply.github.com>
2023-03-26 11:53:54 +0545

    Add files via upload

---
+++ steps.sh
@@ -0,0 +1,8 @@
+git status
+git init
+git config --global user.name"youe username"
+git config --global user.email"youremail@gmail.com"
+git add index.html
+git status
+git commit -m "Mero first commit"
+git log
